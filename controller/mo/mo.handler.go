1364293483b1bc51b26ef4ddd7485a4e65d5fede
gujarats <gujarat.santana@gmail.com>
2017-04-07 23:23:19 +0700

    remove logger

--- controller/mo/mo.handler.go
+++ controller/mo/mo.handler.go
@@ -1,24 +1,13 @@
 package mo
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/Gujarats/send-program/model/global"
 	"github.com/Gujarats/send-program/model/mo"
 	"github.com/Gujarats/send-program/util"
 	"github.com/Gujarats/send-program/util/token"
 )
-
-// create logger to print error in the console
-var logger *log.Logger
-
-func init() {
-	logger = log.New(os.Stderr,
-		"Controller Driver :: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-}
 
 func SendMo(moModel mo.Mo) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
