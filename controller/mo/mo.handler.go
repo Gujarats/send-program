cf2ff25124f7434ddcb6790f681b32b96063bdab
gujarats <gujarat.santana@gmail.com>
2017-04-11 09:40:18 +0700

    add more handler

--- controller/mo/mo.handler.go
+++ controller/mo/mo.handler.go
@@ -2,6 +2,7 @@ package mo
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/Gujarats/send-program/model/global"
 	"github.com/Gujarats/send-program/model/mo"
@@ -11,6 +12,7 @@ import (
 
 func SendMo(moModel mo.Mo) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
 		msisdn := r.FormValue("msisdn")
 		operatorId := r.FormValue("operatorid")
 		shortCodeId := r.FormValue("shortcodeid")
@@ -33,7 +35,26 @@ func SendMo(moModel mo.Mo) http.Handler {
 			moModel.InsertData(token)
 		}(moModel)
 
-		global.SetOkResponse(w, "Ok", "Data saved", nil)
+		elapsed := time.Since(startTime).Seconds()
+		global.SetOkResponse(w, "Ok", "Data saved", elapsed, nil)
+
+	})
+}
+
+func StatMo(moModel mo.Mo) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		date := r.FormValue("date")
+
+		if !util.CheckValue(date) {
+
+			global.SetBadResponse(w, "Failed", "Empty Params")
+			return
+		}
+
+		result := moModel.GetStats(date)
+
+		global.SetOkResponse(w, "Ok", "Successfully", time.Since(startTime).Seconds(), result)
 
 	})
 }
